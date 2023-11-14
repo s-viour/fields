a680e0f9260a0912e7d657100ce56d4b7d5fdad0
matt wyatt <mwyatt1000@gmail.com>
2023-11-14 03:30:45 -0500

    support passing config file via CLI

--- src/fields/__main__.py
+++ src/fields/__main__.py
@@ -2,6 +2,7 @@ import os
 import sys
 import json
 import logging
+import argparse
 from .bot import Fields
 from dotenv import load_dotenv
 import discord
@@ -13,18 +14,21 @@ def main():
     """parses the config and environment, then starts the bot"""
     load_dotenv()
 
+    args = _parse_args()
+    _setup_logging(args.debug)
+
     token = os.getenv("BOT_TOKEN")
     if token is None:
         log.warning("missing `BOT_TOKEN` environment variable")
         return
 
     try:
-        cfg = _read_config()
+        cfg = _read_config(args.config)
     except KeyError as e:
         print(f"invalid config: {e}", file=sys.stderr)
         return
     except FileNotFoundError:
-        print("could not find `config.json`", file=sys.stderr)
+        print(f"could not find `{args.config}`", file=sys.stderr)
         return
 
     intents = discord.Intents.default()
@@ -39,11 +43,23 @@ def main():
     client.run(token, log_handler=None)
 
 
-def _read_config():
-    """read and validates the `config.json` file. assumes that the file exists.
+def _parse_args():
+    """handles the creation of and execution of the `ArgumentParser`"""
+    parser = argparse.ArgumentParser(
+        prog="fields",
+        description="discord bot that plays ambience. forever",
+    )
+
+    parser.add_argument("-c", "--config", default="config.json")
+    parser.add_argument("-d", "--debug", action="store_true")
+    return parser.parse_args()
+
+
+def _read_config(filename):
+    """read and validates the passed config file. assumes that the file exists.
     if the file does not exist, `open` will throw a `FileNotFoundError`
     """
-    with open("config.json") as f:
+    with open(filename) as f:
         j = json.load(f)
         if "channelIDs" not in j or "audioPath" not in j:
             raise KeyError("missing key `channelIDs`")
@@ -63,6 +79,4 @@ def _setup_logging(debug=False):
 
 
 if __name__ == "__main__":
-    debug = "--debug" in sys.argv or "-d" in sys.argv
-    _setup_logging(debug)
     main()
